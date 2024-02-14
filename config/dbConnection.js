267fe78a238cf2334b0e6b5d6c5e7a68172578bc
neelmaru123 <nmaru094@gamil.com>
2024-02-14 17:56:22 +0530

    DB changed

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://neelmaru63:neelmaru63@cluster0.ewl7mj7.mongodb.net/?retryWrites=true&w=majority", {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
+    console.log('Database connected successfully');
   } catch (err) {
-    console.error(err);
+    console.error('Database connection error', err);
   }
 };
 
