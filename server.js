5d944602f41b52833a31b56db099950cd6716908
neelmaru123 <nmaru094@gamil.com>
2024-03-01 18:46:20 +0530

    server.js is changed

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const cors = require("cors");
 
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConnection");
+const Bill = require("./model/Bill");
 
 const PORT = process.env.PORT || 3500;
 
@@ -22,10 +23,12 @@ connectDB();
 app.use(cors());
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
@@ -33,10 +36,80 @@ app.use("/", express.static(path.join(__dirname, "public")));
 
 app.post("/login", handleLogin);
 
-app.use("/bill", require("./routes/bill"));
-app.use("/expense", require("./routes/expense"));
+
 
 mongoose.connection.once("open", () => {
   console.log("connected to mongoDB");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+
+app.post("/bill", async (req, res) => {
+  // console.log(req.body.billNo, req.body.companyName);
+  console.log(req.body);
+  if (!req?.body?.billNo || !req?.body?.companyName) {
+    return res
+      .status(400)
+      .json({ message: "BillNo and companyName is required" });;
+  }
+
+  const newBill = {};
+  if (req.body?.billNo) newBill.billNo = req.body.billNo;
+  if (req.body?.companyName) newBill.companyName = req.body.companyName;
+  if (req.body?.productDetails) newBill.productDetails = req.body.productDetails;
+  if (req.body?.date) newBill.date = req.body.date;
+  if (req.body?.totalAmount) newBill.totalAmount = req.body.totalAmount;
+  if (req.body?.gst) newBill.gst = req.body.gst;
+  if (req.body?.totalBillAmount) newBill.totalBillAmount = req.body.totalBillAmount;
+
+  try {
+    const result = await Bill.create(newBill);
+    res.status(201).json({ success: true });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 });
+
+app.get("/bill", async (req, res) => {
+  console.log("getAllBills");
+  const bills = await Bill.find();
+  if (!bills) {
+    return res.status(204).json({ message: "No Bill Found!" });
+  }
+  res.json(bills);
+});
+
+app.get("/bill/:id", async (req, res) => {
+  if (!req?.params?.id) {
+    return res.status(400).json({ message: "ID is required" });
+  }
+
+  const bill = await Bill.findOne({ _id: req.params.id }).exec();
+
+  if (!bill) {
+    return res
+      .status(204)
+      .json({ message: `ID: ${req.params.id} does not match` });
+  }
+
+  res.json(bill);
+});
+
+app.delete("/bill/:id", async (req, res) => {
+  if (!req?.params?.id) {
+    return res.status(400).json({ message: "ID is required" });
+  }
+
+  const bill = await Bill.findOne({ _id: req.params.id }).exec();
+
+  if (!bill) {
+    return res
+      .status(204)
+      .json({ message: `ID: ${req.params.id} does not match` });
+  }
+
+  const result = await bill.deleteOne();
+  res.json({ success: true, result: result });
+});
+
+// app.use("/bill", require("./routes/bill"));
+// app.use("/expense", require("./routes/expense"));
